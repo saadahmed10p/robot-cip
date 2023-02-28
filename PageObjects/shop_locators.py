376c6ce2a16d70fcfa417e2d702fcd902c987b68
saadahmed10p <saad.ahmed@10pearls.com>
2023-02-28 13:46:25 +0500

    Added locators fdor name and email of recipient

--- PageObjects/shop_locators.py
+++ PageObjects/shop_locators.py
@@ -1,2 +1,4 @@
 product1 = "//body/div[4]/div[1]/div[4]/div[3]/div[1]/div[1]/div[3]/div[2]/div[1]/div[2]/div[3]/div[2]/input[1]"
 pdt1_addToCart = "id:add-to-cart-button-2"
+recipient_name = "id:giftcard_2_RecipientName"
+recipient_email =  "id:giftcard_2_RecipientEmail"
