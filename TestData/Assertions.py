17b025a572437d915e433147bf5bc107ef3ea6b3
saadahmed10p <saad.ahmed@10pearls.com>
2023-04-20 18:49:45 +0500

    Added condition to fetch data from json

---
+++ TestData/Assertions.py
@@ -0,0 +1,3 @@
+invalid_creds_msg = "Username and password do not match any user in this service"
+locked_user_msg = "Sorry, this user has been locked out."
+incorrect_user_details_msg = "First Name is required"
